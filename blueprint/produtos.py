e03b76274f1e36b99b195754c3e3ac265e94a0a3
Creuma Kuzola <creumakuzola@gmail.com>
2022-02-08 23:51:50 +0100

    Renderizacao automatica adicionada na pagina produtos

--- blueprint/produtos.py
+++ blueprint/produtos.py
@@ -1,7 +1,23 @@
 from flask import render_template, Blueprint
+from conexao import start_connection_db, close_connection_db
 
 produtos = Blueprint('produtos',__name__,static_folder='static',template_folder='templates')
 
 @produtos.route('')
 def produto():
-    return render_template('produtos.html')
+    cursor, conexao = start_connection_db()
+
+    cursor.execute(f'SELECT * FROM produto WHERE seccao_produto=%s',('promocao',))
+    promocao = cursor.fetchall()
+
+    cursor.execute(f'SELECT * FROM produto WHERE seccao_produto=%s',('novidades',))
+    novidades = cursor.fetchall()
+
+    cursor.execute(f'SELECT * FROM produto WHERE seccao_produto=%s',('mais vendidos',))
+    mais_vendidos = cursor.fetchall()
+
+    cursor.execute(f'SELECT * FROM produto WHERE seccao_produto=%s',('produtos',))
+    produto1 = cursor.fetchall()
+
+    close_connection_db(cursor,conexao)
+    return render_template('produtos.html',promocao=promocao,novidades=novidades,mais_vendidos=mais_vendidos,produto1=produto1,image_static_url='/static/img/')
